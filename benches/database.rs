1c8ff5e65c80ba207e7e1cd53a06cd98658b6e10
766974616c79 <100430077+766974616c79@users.noreply.github.com>
2025-11-05 09:41:17 +0100

    Try to improve the benchmark quality again

--- benches/database.rs
+++ benches/database.rs
@@ -6,16 +6,16 @@ use zeta::Database;
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("database");
     group.sample_size(1000);
-    group.measurement_time(Duration::from_secs(60));
+    group.measurement_time(Duration::from_secs(120));
 
     let mut database = Database::default();
-    group.bench_function("database insert", |b| b.iter(||       database.insert(String::from(
+    group.bench_function("insert", |b| b.iter(||       database.insert(String::from(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit justo a magna dapibus, in porta ipsum auctor. Proin in ornare est. Vivamus vestibulum felis orci, at mattis nisl consequat in. Sed laoreet pretium urna, id volutpat libero vulputate ac. Aliquam tempus ex ac dolor dignissim ornare. Nullam vel nisl leo. Pellentesque sed justo tortor. Donec id quam arcu.",
     ))));
 
     database.insert(String::from("hello"));
 
-    group.bench_function("database get", |b| b.iter(|| database.get("hello")));
+    group.bench_function("get", |b| b.iter(|| database.get("hello")));
     group.finish();
 }
 
