0a3468641cbeae7930b95d519e355bec58c7003f
766974616c79 <100430077+766974616c79@users.noreply.github.com>
2025-11-05 09:34:52 +0100

    Try to improve the benchmark quality

--- benches/database.rs
+++ benches/database.rs
@@ -1,15 +1,22 @@
+use std::time::Duration;
+
 use criterion::{Criterion, criterion_group, criterion_main};
 use zeta::Database;
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("database");
+    group.sample_size(1000);
+    group.measurement_time(Duration::from_secs(60));
+
     let mut database = Database::default();
-    c.bench_function("database insert", |b| b.iter(||       database.insert(String::from(
+    group.bench_function("database insert", |b| b.iter(||       database.insert(String::from(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit justo a magna dapibus, in porta ipsum auctor. Proin in ornare est. Vivamus vestibulum felis orci, at mattis nisl consequat in. Sed laoreet pretium urna, id volutpat libero vulputate ac. Aliquam tempus ex ac dolor dignissim ornare. Nullam vel nisl leo. Pellentesque sed justo tortor. Donec id quam arcu.",
     ))));
 
     database.insert(String::from("hello"));
 
-    c.bench_function("database get", |b| b.iter(|| database.get("hello")));
+    group.bench_function("database get", |b| b.iter(|| database.get("hello")));
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
